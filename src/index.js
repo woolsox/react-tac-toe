b586c6b003d7278538f7eb944471021730dd6ce9
woolsox <dimeo.colin@gmail.com>
2018-04-06 16:11:15 -0400

    [wip] pick back up @ renderSquare method to resolve error (1/3 the way thru tut)

--- src/index.js
+++ src/index.js
@@ -3,25 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSqaure(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
