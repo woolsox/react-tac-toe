bb26fca5709de08ebcb70c1a5fcf5d73820b853d
woolsox <dimeo.colin@gmail.com>
2018-04-09 16:56:10 -0400

    updates Board class to handle X & O turn toggle

--- src/index.js
+++ src/index.js
@@ -14,14 +14,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   };
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSqaure(i) {
@@ -29,7 +33,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next Player: X';
+    const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (<div>
       <div className="status">{status}</div>
